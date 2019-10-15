e8dacb301e86436dd5b2e324032eeda889eaedee
SherSpock <wrtimberlake@gmail.com>
2019-10-15 10:54:48 -0400

    Clarify variable names
    Make yourselves at home!
    
    `Puppy_results` and `twitter_api` replace `results` and `api`

--- the_right_tweets.py
+++ the_right_tweets.py
@@ -1,10 +1,10 @@
 import twitter, os
 
-api = twitter.Api(consumer_key=os.getenv('CONSUMER_KEY'), 
+twitter_api = twitter.Api(consumer_key=os.getenv('CONSUMER_KEY'), 
                   consumer_secret=os.getenv('CONSUMER_SECRET'),
                   access_token_key=os.getenv('ACCESS_TOKEN'),
                   access_token_secret=os.getenv('ACCESS_TOKEN_SECRET'))
 
-results = api.GetSearch(term='puppies')
 
 print(results)
+puppy_results = twitter_api.GetSearch(term='puppies', count=1)
