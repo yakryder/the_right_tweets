2d72d7fd8fbf90221dc695f0a9922e631ffbb1e8
SherSpock <wrtimberlake@gmail.com>
2019-10-15 10:55:41 -0400

    Print text of first tweet

--- the_right_tweets.py
+++ the_right_tweets.py
@@ -5,6 +5,6 @@ twitter_api = twitter.Api(consumer_key=os.getenv('CONSUMER_KEY'),
                   access_token_key=os.getenv('ACCESS_TOKEN'),
                   access_token_secret=os.getenv('ACCESS_TOKEN_SECRET'))
 
-
-print(results)
 puppy_results = twitter_api.GetSearch(term='puppies', count=1)
+puppy_tweet = puppy_results[0]
+print(puppy_tweet.text)
